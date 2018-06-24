fc80fd77077b272c183a9982d7ee7e2c9602683d
Mohammed Avez <mohammedavez85@gmail.com>
2018-06-24 16:22:41 +0530

    solved responsive problem and now it is lit

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 	<title>Select Team</title>
-
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/indexstyle.css">
-	<meta charset="UTF-8">
-	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	
 </head>
 <body>
 	<form action="#" method="post">
--- next.php
+++ next.php
@@ -1,47 +1,119 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Team-1</title>
-	<link rel="stylesheet" type="text/css" href="css/set-team-1.css">
+	<title>Team-1</title> 
+
+
+	<link rel="stylesheet" type="text/css" href="css/set-team-1.css"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<form action="#" method="post">
 <div class="container">
-	
-		<div class="player-1">
+	<div class="heading">
+   <h2>Set the Team</h2> 
+  </div>
+		<div class="player-1 players">
 			<input type="text" name="player-1">
-		</div>
-		<div class="player-2">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
+	 </div>
+		<div class="player-2 players">
 			<input type="text" name="player-2">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-3">
+		<div class="player-3 players">
 			<input type="text" name="player-3">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-4">
+		<div class="player-4 players">
 			<input type="text" name="player-4">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-5">
+		<div class="player-5 players">
 			<input type="text" name="player-5">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-6">
+		<div class="player-6 players">
 			<input type="text" name="player-6">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-7">
+		<div class="player-7 players">
 			<input type="text" name="player-7">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-8">
+		<div class="player-8 players">
 			<input type="text" name="player-8">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-9">
+		<div class="player-9 players">
 			<input type="text" name="player-9">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-10">
+		<div class="player-10 players">
 			<input type="text" name="player-10">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
-		<div class="player-11">
+		<div class="player-11 players">
 			<input type="text" name="player-11">
+			<select class="skill">
+     			<option>Select Team:</option>
+    			<option>Bats men</option>
+   	 			<option>Bowler</option>
+    			<option >All rounder</option>
+  			</select>
 		</div>
 	
-</div></form>
+</div>
+</form>
 </body>
 </html>
